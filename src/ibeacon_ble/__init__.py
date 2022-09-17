072399ee5492a59a3dacf7ea6cfdf0842b7a3694
J. Nick Koston <nick@koston.org>
2022-09-17 13:36:23 -0500

    feat: port the parser

--- src/ibeacon_ble/__init__.py
+++ src/ibeacon_ble/__init__.py
@@ -1,6 +1,13 @@
 __version__ = "0.1.0"
+import struct
 from dataclasses import dataclass
+from uuid import UUID
 
+from home_assistant_bluetooth import BluetoothServiceInfo
+
+UNPACK_IBEACON = struct.Struct(">HHb").unpack
+
+IBEACON_MFR_ID = 76
 __all__ = ["parse", "iBeaconAdvertisement"]
 
 
@@ -8,16 +15,32 @@ __all__ = ["parse", "iBeaconAdvertisement"]
 class iBeaconAdvertisement:
     """A dataclass for iBeacon BLE advertisements."""
 
-    uuid: str
+    uuid: UUID
     major: int
     minor: int
     power: int
+    cypress_temperature: float
+    cypress_humidity: float
+    rssi: int
+    source: str
 
 
-def parse() -> iBeaconAdvertisement:
+def parse(service_info: BluetoothServiceInfo) -> iBeaconAdvertisement | None:
+    if IBEACON_MFR_ID not in service_info.manufacturer_data:
+        return None
+    data = service_info.manufacturer_data[IBEACON_MFR_ID]
+
+    # Thanks to https://github.com/custom-components/ble_monitor/blob/master/custom_components/ble_monitor/ble_parser/ibeacon.py
+    uuid = data[2:18]
+    (major, minor, power) = UNPACK_IBEACON(data[18:23])
+
     return iBeaconAdvertisement(
-        uuid="00000000-0000-0000-0000-000000000000",
-        major=0,
-        minor=0,
-        power=0,
+        uuid=UUID("".join(f"{i:02X}" for i in uuid)),
+        major=major,
+        minor=minor,
+        power=power,
+        cypress_temperature=175.72 * ((minor & 0xFF) * 256) / 65536 - 46.85,
+        cypress_humidity=125 * ((major & 0xFF) * 256) / 65536 - 6.0,
+        rssi=service_info.rssi,
+        source=service_info.source,
     )
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,5 +1,44 @@
+from uuid import UUID
+
+from home_assistant_bluetooth import BluetoothServiceInfo
+
 from ibeacon_ble import iBeaconAdvertisement, parse
+
+SERVICE_INFO = BluetoothServiceInfo(
+    address="00:00:00:00:00:00",
+    rssi=-60,
+    name="BlueCharm_177999",
+    manufacturer_data={76: b"\x02\x15BlueCharmBeacons\x0e\xfe\x13U\xc5"},
+    service_data={
+        "00002080-0000-1000-8000-00805f9b34fb": b"k\x0c\x0e\xfe\x13U",
+        "0000feaa-0000-1000-8000-00805f9b34fb": b" \x00\x0b\xfe\x1c\x00\x00\x00\x07\x08\x00\x00>|",
+    },
+    service_uuids=["0000feaa-0000-1000-8000-00805f9b34fb"],
+    source="hci0",
+)
+NOT_IBEACON_SERVIE_INFO = BluetoothServiceInfo(
+    address="00:00:00:00:00:00",
+    rssi=-60,
+    name="not",
+    manufacturer_data={21: b"\x02\x15BlueCharmBeacons\x0e\xfe\x13U\xc5"},
+    service_data={},
+    service_uuids=[],
+    source="hci0",
+)
 
 
 def test_parse():
-    isinstance(parse(), iBeaconAdvertisement)
+    parsed = parse(SERVICE_INFO)
+    assert isinstance(parsed, iBeaconAdvertisement)
+    assert parsed.cypress_humidity == 118.0234375
+    assert parsed.cypress_temperature == 11.494531250000001
+    assert parsed.major == 3838
+    assert parsed.minor == 4949
+    assert parsed.power == -59
+    assert parsed.rssi == -60
+    assert parsed.uuid == UUID("426c7565-4368-6172-6d42-6561636f6e73")
+
+
+def test_not_parse():
+    parsed = parse(NOT_IBEACON_SERVIE_INFO)
+    assert parsed is None
