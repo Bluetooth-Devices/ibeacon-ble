ae71fbdebba18f0fbaf1dcdf6db135eb6e86edc2
J. Nick Koston <nick@koston.org>
2022-09-23 08:37:59 -1000

    fix: clamp distance to max theoretical value (#12)

--- tests/test_init.py
+++ tests/test_init.py
@@ -77,8 +77,9 @@ def test_not_parse_short_service_info():
 def tests_calculate_distance_meters():
     assert calculate_distance_meters(-59, -60) == 1.1352362990362899
     assert calculate_distance_meters(59, -60) == 1.183020818815412
-    assert calculate_distance_meters(12, -80) is None
+    assert calculate_distance_meters(12, -80) == 400.0
     assert calculate_distance_meters(59, 0) is None
-    assert calculate_distance_meters(-3, -100) is None
+    assert calculate_distance_meters(-3, -100) == 400.0
+    assert calculate_distance_meters(-3, -96) == 400.0
     assert calculate_distance_meters(-3, -3) == 1.01076
     assert calculate_distance_meters(-4, -3) == 0.056313514709472656
