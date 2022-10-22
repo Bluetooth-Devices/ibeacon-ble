38f8fd6455e327f3c739061370a1da4fe3f1c3eb
J. Nick Koston <nick@koston.org>
2022-10-22 11:59:33 -0500

    feat: provide vendor information and label transient devices
    BREAKING CHANGE: The parser is now a bound method as iBeaconParser.parse

--- src/ibeacon_ble/__init__.py
+++ src/ibeacon_ble/__init__.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
 
+import asyncio
+import contextlib
 import struct
 from dataclasses import dataclass
 from typing import cast
 from uuid import UUID
 
+import aiohttp
 from home_assistant_bluetooth import BluetoothServiceInfo
+from mac_vendor_lookup import AsyncMacLookup
 
 UNPACK_IBEACON = struct.Struct(">HHb").unpack
 
@@ -52,8 +56,10 @@ class iBeaconAdvertisement:
     cypress_temperature: float
     cypress_humidity: float
     rssi: int
+    transient: bool
     source: str
     distance: int | None
+    vendor: str | None
 
     def update_rssi(self, rssi: int) -> None:
         """Update the RSSI and distance."""
@@ -74,46 +80,73 @@ def is_ibeacon_service_info(service_info: BluetoothServiceInfo) -> bool:
     return True
 
 
-def parse(service_info: BluetoothServiceInfo) -> iBeaconAdvertisement | None:
-    if not is_ibeacon_service_info(service_info):
-        return None
+class iBeaconParser:
+    """Parse iBeacon BLE advertisements."""
 
-    data = service_info.manufacturer_data[APPLE_MFR_ID]
-    # Thanks to https://github.com/custom-components/ble_monitor/blob/master/custom_components/ble_monitor/ble_parser/ibeacon.py
-    uuid = data[2:18]
-    uuid_str = "".join(f"{i:02X}" for i in uuid)
+    def __init__(self) -> None:
+        """Initialize the parser."""
+        self._mac_vendor_lookup: AsyncMacLookup | None = None
 
-    # Tilt Hydrometers and devices that spew random major/minor values
-    # We don't actually need to ban them since the integration will eventually
-    # filter them out, but this is a nice optimization.
-    if uuid_str in BANNED_UUIDS:
-        return None
+    async def async_setup(self) -> None:
+        self._mac_vendor_lookup = AsyncMacLookup()
+        with contextlib.suppress(asyncio.TimeoutError, aiohttp.ClientError):
+            # We don't care if this fails since it only
+            # improves the data we get.
+            await self._mac_vendor_lookup.load_vendors()
 
-    name = service_info.name
-    if len(name) == 18 and name[0] == "S" and name[17] == "C":
-        # If it starts with S and ends with C and the inner
-        # 16 characters are hex, it's a random transient
-        try:
-            int(name[1:17], 16)
+    def _async_get_vendor(self, mac_address: str) -> str | None:
+        """Lookup the vendor."""
+        assert self._mac_vendor_lookup is not None  # nosec
+        oui = self._mac_vendor_lookup.sanitise(mac_address)[:6]
+        return self._mac_vendor_lookup.prefixes.get(oui)
+
+    def parse(self, service_info: BluetoothServiceInfo) -> iBeaconAdvertisement | None:
+        if not is_ibeacon_service_info(service_info):
             return None
-        except ValueError:
-            pass
 
-    (major, minor, power) = UNPACK_IBEACON(data[18:23])
-    distance = calculate_distance_meters(power, service_info.rssi)
+        data = service_info.manufacturer_data[APPLE_MFR_ID]
+        # Thanks to https://github.com/custom-components/ble_monitor/blob/master/custom_components/ble_monitor/ble_parser/ibeacon.py
+        uuid = data[2:18]
+        uuid_str = "".join(f"{i:02X}" for i in uuid)
 
-    return iBeaconAdvertisement(
-        name=service_info.name,
-        uuid=UUID(uuid_str),
-        major=major,
-        minor=minor,
-        power=power,
-        cypress_temperature=175.72 * ((minor & 0xFF) * 256) / 65536 - 46.85,
-        cypress_humidity=125 * ((major & 0xFF) * 256) / 65536 - 6.0,
-        rssi=service_info.rssi,
-        source=service_info.source,
-        distance=int(round(distance)) if distance is not None else None,
-    )
+        # Tilt Hydrometers and devices that spew random major/minor values
+        # We don't actually need to ban them since the integration will eventually
+        # filter them out, but this is a nice optimization.
+        if uuid_str in BANNED_UUIDS:
+            return None
+
+        name = service_info.name
+        transient = False
+        if len(name) == 18 and name[0] == "S" and name[17] == "C":
+            # If it starts with S and ends with C and the inner
+            # 16 characters are hex, it's a random transient
+            try:
+                int(name[1:17], 16)
+                transient = True
+            except ValueError:
+                pass
+
+        (major, minor, power) = UNPACK_IBEACON(data[18:23])
+        distance = calculate_distance_meters(power, service_info.rssi)
+
+        vendor = None
+        if ":" in service_info.address:
+            vendor = self._async_get_vendor(service_info.address)
+
+        return iBeaconAdvertisement(
+            name=service_info.name,
+            uuid=UUID(uuid_str),
+            major=major,
+            minor=minor,
+            power=power,
+            cypress_temperature=175.72 * ((minor & 0xFF) * 256) / 65536 - 46.85,
+            cypress_humidity=125 * ((major & 0xFF) * 256) / 65536 - 6.0,
+            rssi=service_info.rssi,
+            source=service_info.source,
+            transient=transient,
+            vendor=vendor,
+            distance=int(round(distance)) if distance is not None else None,
+        )
 
 
 def calculate_distance_meters(power: int, rssi: int) -> float | None:
